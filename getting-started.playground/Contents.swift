299e450a6a4c84e07178399d97699b095f8532d2
Tyler Knapp <tgknapp11@gmail.com>
2020-04-13 10:38:59 -0700

    structures

--- getting-started.playground/Contents.swift
+++ getting-started.playground/Contents.swift
@@ -50,17 +50,46 @@ let people: Array = ["Tyler", "Anna", "Alex", "Dana"]
 //    print(person)
 //}
 
-let legend = ["color": "blue", "object": "water"]
+//let legend = ["color": "blue", "object": "water"]
+//
+//for num in stride(from: 0, to: 10, by: 2) {
+//    print(num)
+//}
+//
+//for item in legend {
+//    print(item.key)
+//    print(item.value)
+//}
+//
+//for (index, value) in people.enumerated() {
+//    print("\(index): \(value)")
+//}
+//
 
-for num in stride(from: 0, to: 10, by: 2) {
-    print(num)
+//var n = 0
+//
+//while (n < people.count) {
+//    print(people[n])
+//    n += 1
+//}
+
+func add(num1: Int, num2: Int) -> Int {
+    return num1 + num2
 }
 
-for item in legend {
-    print(item.key)
-    print(item.value)
+print(add(num1: 1, num2: 2))
+
+struct person {
+    var name: String
+    var age: Int
+    var email: String
+    var password: String
 }
 
-for (index, value) in people.enumerated() {
-    print("\(index): \(value)")
+let user1 = person(name: "Tyler", age: 24, email: "tgknapp11@gmail.com", password: "secret")
+
+func printUserInfo(user: person) -> Any {
+    return (user.name)
 }
+
+print(printUserInfo(user: user1))
